bdb9eb5ffb0c54a650c599bd675651e29c1e3e06
Nartyna <mplutwska@gmail.com>
2022-11-29 20:15:32 +0100

    Add forward declaration in BasePawn.h and create capsule colision shape and made it a root component

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -18,8 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
+	class UCapsuleComponent* CapsuleComp;
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
