b0f3181c4125f1a5e347980b5bf8ff82a1c8dac4
Nartyna <mplutwska@gmail.com>
2022-12-17 19:11:38 +0100

    Add new health component class, add it to bp_tank and bp_tower, bind DamageTaken func to 'OnTakeAnyDamage' on health component

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -29,7 +29,7 @@ void AProjectile::BeginPlay()
 
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("HitComp name: %s"), *HitComp->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("OtherActor name: %s"), *OtherActor->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("OtherComp name: %s"), *OtherComp->GetName());
+	// UE_LOG(LogTemp, Warning, TEXT("HitComp name: %s"), *HitComp->GetName());
+	// UE_LOG(LogTemp, Warning, TEXT("OtherActor name: %s"), *OtherActor->GetName());
+	// UE_LOG(LogTemp, Warning, TEXT("OtherComp name: %s"), *OtherComp->GetName());
 }
